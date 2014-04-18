a87163555f4bdfd33895443f54c8f52d8b005e6c
sk <xomamail@gmail.com>
2014-04-18 11:35:30 +0300

    fix Ah and Wh measure, change button to interrupt

--- ebikeMeter/ebikeMeter.ino
+++ ebikeMeter/ebikeMeter.ino
@@ -6,7 +6,6 @@ int batMonPin = A5;         // input pin for the voltage divider
 int batVal = 0;             // variable for the A/D value
 
 int backlightPin = 9;
-//int keyPin = 8;
 float pinVoltage = 0;       // variable to hold the calculated voltage
 float batteryVoltage = 0;
 float totalVoltage = 0;
@@ -15,14 +14,13 @@ float avgVoltage = 0;
 int analogInPin = A4;       // Analog input pin that the carrier board OUT is connected to
 int sensorValue = 0;        // value read from the carrier board
 float outputValue = 0;      // output in milliamps
-unsigned long msec = 0;
 unsigned long prevMsec = 0;
 unsigned long timeLed = 0;
-float time = 0.0;
+unsigned long measureTime = 0;
+unsigned long prevMeasureTime = 0;
 int sample = 0;
-float totalCharge = 0.0;
-float averageAmps = 0.0;
 float ampSeconds = 0.0;
+float miliAmpSeconds = 0.0;
 float ampHours = 0.0;
 float wattHours = 0.0;
 float amps = 0.0;
@@ -33,9 +31,11 @@ boolean on_off_led = true;
 boolean saved = false;
 boolean led = false;
 
+boolean serialDebug = false; //enable serial debug
+
 //EEPROM addreses
 byte ledAddr = 0;
-byte msecAddr = 1;
+byte ampSecondsAddr = 1;
 byte totalChargeAddr = 5;
 byte sampleAddr = 10;
 byte distanceAddr = 15;
@@ -50,28 +50,23 @@ volatile float speedValue = 0;
 volatile float distance = 0;
 volatile float totalDistance = 0;
 volatile float WHEEL = 2.070;
-volatile int i = 0;
+volatile unsigned long i = 0;
 
 float ratio = 0;  // Calculated from R1 / R2
-//test branch
+//exp branch
 void setup() {
+    Serial.begin(9600);
     attachInterrupt(0, speed, FALLING); //interrupt for speed sensor
 
-    attachInterrupt(1, button, FALLING); //interrupt for button
+    attachInterrupt(1, button, LOW); //interrupt for button
 
     Display.Setup();
     Display.Clear();
 
-    pinMode(7, OUTPUT);
-    digitalWrite(7, HIGH);
-
     pinMode(backlightPin, OUTPUT);                        // backlight pin
 
-    //pinMode(keyPin, INPUT);
-
     on_off_led =  EEPROM.read(ledAddr);
-    msec = EEPROM_float_read(msecAddr);                   //read time
-    totalCharge = EEPROM_float_read(totalChargeAddr);     //read totalCharge
+    ampSeconds = EEPROM_float_read(ampSecondsAddr);                   //read amp sec
     sample = EEPROM_float_read(sampleAddr);               //read sample
     distance = EEPROM_float_read(distanceAddr);           //read distance
     totalDistance = EEPROM_float_read(totalDistanceAddr); //read totalDistance
@@ -90,7 +85,16 @@ void speed(){
 }
 
 void button(){
-	i++;
+    i++;
+
+    if(i>220000){
+        Display.Clear();
+        Display.setTextColor(1);
+        Display.setTextSize(2);
+        Display.setCursor(5,0);
+        Display.print("RESET");
+        Display.Update();
+    }
 }
 
 void loop() {
@@ -100,12 +104,29 @@ void loop() {
     int sampleAmpVal = 0;
     int avgSAV = 0;
 
+    measureTime = millis() - prevMeasureTime;
+
+    prevMeasureTime = millis();
+
     //button check
-    if((i>0 && i<2000)&&(millis() - prevMillis < 10000)){
+    if((i>0 && i<220000)&&(millis() - prevMillis < 10000)){
         on_off_led =!on_off_led;
         i=0;
+        Display.Clear();
+        Display.setTextColor(1);
+        Display.setTextSize(2);
+        Display.setCursor(10,0);
+        if(on_off_led){
+            digitalWrite(backlightPin, HIGH);
+            Display.print("LED ON");
+        }else{
+            Display.print("LED OFF");
+        }
+        Display.Update();
+        delay(1000);
     }
-    if(i>2000){
+    
+    if(i>220000){
         resetMemory();
         i = 0;
     }
@@ -117,14 +138,14 @@ void loop() {
     if(millis() - prevMillis > 10000){
 
         if(!saved){
-             saveData();                   //save data
-             delay(500);
-             ledState = false;
+            saveData();                   //save data
+            delay(500);
+            ledState = false;
         }
 
         saved = true;
 
-        if(i>0 && i<2000){
+        if(i>0 && i<220000){
             timeLed = millis()+10000;
             i=0;
         }
@@ -152,7 +173,6 @@ void loop() {
     for(int x = 0; x < 10; x++){
         sensorValue = analogRead(analogInPin) - 512;
         sampleAmpVal = sampleAmpVal + sensorValue; // add samples together
-
         batVal = analogRead(batMonPin);    // read the voltage on the divider
         sampleBVal = sampleBVal + batVal; // add samples together
 
@@ -161,36 +181,30 @@ void loop() {
 
     avgSAV = sampleAmpVal / 10;
 
-    outputValue = (avgSAV*4.98)/66;
+    outputValue = (avgSAV*4.98)/61;
 
     avgBVal = sampleBVal / 10; //divide by 10 (number of samples) to get a steady reading
 
-    pinVoltage = avgBVal * 0.005265;
+    pinVoltage = avgBVal * 0.00532;
 
     ratio = (float)R1 / (float)R2;
     batteryVoltage = pinVoltage * ratio;    //  Use the ratio calculated for the voltage divider
                                             //  to calculate the battery voltage
-
 
     amps = (float) outputValue;
     amps = abs(amps);
     watts = amps * batteryVoltage;
 
     sample = sample + 1;
+
     prevMsec = millis()-prevMsec;
-    msec = msec + prevMsec;
 
     totalVoltage = totalVoltage + batteryVoltage;
 
     avgVoltage = totalVoltage / sample;
 
-    time = (float) msec / 1000.0;
-
-    totalCharge = totalCharge + amps;
-
-    averageAmps = totalCharge / sample;
-
-    ampSeconds = averageAmps*time;
+    miliAmpSeconds = amps*measureTime;
+    ampSeconds = ampSeconds + miliAmpSeconds/1000;
 
     ampHours = ampSeconds/3600;
 
@@ -204,7 +218,31 @@ void loop() {
     // for the analog-to-digital converter to settle
     // after the last reading:
     delay(10);
+    if(serialDebug){
+        debug();
+    }
     prevMsec = millis();
+}
+
+void debug(){
+    Serial.print("V=");
+    Serial.print(batteryVoltage);
+    Serial.print("  A=");
+    Serial.print(amps);
+    Serial.print("  W=");
+    Serial.print(watts);
+    Serial.print("  Ah=");
+    Serial.print(ampHours);
+    Serial.print("  Wh=");
+    Serial.print(wattHours);
+    Serial.print("  time:");
+    Serial.print(millis()/1000);
+    Serial.print("  mT=");
+    Serial.print(measureTime);
+    Serial.print("  ampSensor:");
+    Serial.print(sensorValue);
+
+    Serial.println();
 }
 
 void EEPROM_float_write(int addr, float val){ // write to ЕЕРRОМ
@@ -257,7 +295,6 @@ void display(){
     Display.print("A");
 
     Display.setTextColor(1);
-
     Display.setTextSize(2);
 
     Display.setCursor(0,43);
@@ -276,10 +313,10 @@ void display(){
     Display.fillRoundRect(0,58,95,10,0,1);
     Display.setTextColor(0);
     if(on_off_led){
-      Display.setCursor(1,58);
-      Display.print("LED");
+        Display.setCursor(1,58);
+        Display.print("LED");
     }else{
-      Display.print("");
+        Display.print("");
     }
     Display.setCursor(35,58);
     Display.print(totalDistance, 0);
@@ -288,30 +325,21 @@ void display(){
 }
 
 void resetMemory(){
-    EEPROM_float_write(msecAddr, 0);
-    EEPROM_float_write(totalChargeAddr, 0);
+    EEPROM_float_write(ampSecondsAddr, 0);
     EEPROM_float_write(sampleAddr, 0);
     EEPROM_float_write(distanceAddr, 0);
     EEPROM_float_write(totalVoltageAddr, 0);
 
-    msec = 0;
-    totalCharge = 0;
+    ampSeconds = 0;
     sample = 0;
     distance = 0;
     totalVoltage = 0;
-
     Display.Clear();
-    Display.setTextColor(1);
-    Display.setTextSize(2);
-    Display.setCursor(5,0);
-    Display.print("RESET...");
-    Display.Update();
     delay(1000);
 }
 
 void saveData(){
-    EEPROM_float_write(msecAddr, (float)msec); //save time
-    EEPROM_float_write(totalChargeAddr, (float)totalCharge); //save totalCharge
+    EEPROM_float_write(ampSecondsAddr, (float)ampSeconds); //save amp seconds
     EEPROM_float_write(sampleAddr, (float)sample); //save sample
     EEPROM_float_write(distanceAddr, (float)distance); //save distance
     EEPROM_float_write(totalDistanceAddr, totalDistance);
@@ -321,7 +349,7 @@ void saveData(){
     Display.setTextColor(1);
     Display.setTextSize(2);
     Display.setCursor(5,0);
-    Display.print("SAVED...");
+    Display.print("SAVED");
     Display.Update();
     delay(1000);
 }
